9b2cac2b3c3c5e8e5d15185096e43da6afbea79d
von-maurus <mx1807@gmail.com>
2021-08-08 00:15:01 -0400

    Ultimas acciones añadidas a la API
    -Falta conexion con el frontend

--- src/controllers/ventas.controller.js
+++ src/controllers/ventas.controller.js
@@ -1,5 +1,6 @@
 const ventaCtrl = {};
 const Venta = require('../models/venta');
+// ----------Finanzas y Comportamiento----------------
 /**
  * Obtener todas las ventas en un rango de fechas.
  * */
@@ -12,11 +13,12 @@ ventaCtrl.getVentasByDate = async (req, res) => {
     // const zones = await Venta.distinct("table");
     // res.json(zones);
     res.json(ventas);
-}
+};
+
 /**
  * Obtener el total de ingresos en un rango de fechas.
  * */
-ventaCtrl.getTotalVentasByDate = async (req, res) => {
+ventaCtrl.getIngresoTotalByDate = async (req, res) => {
     var inicio = req.body['inicio'];
     var fin = req.body['fin'];
     const montoTotalVentas = await Venta.aggregate(
@@ -47,64 +49,8 @@ ventaCtrl.getTotalVentasByDate = async (req, res) => {
     // res.send('Obtener el total de ganancias en un rango de fecha');
     // var ventas = await Venta.find({ date_opened: { $gte: new Date(inicio + 'Z'), $lte: new Date(fin + 'Z') } });
     // ventasObj = JSON.parse(ventas);
-}
-/**
- * Obtener las ventas atendidas por un mesero en un rango de fechas.
- * */
-ventaCtrl.getWaiterVentasByDate = async (req, res) => {
-    var inicio = req.body['inicio'];
-    var fin = req.body['fin'];
-    var name = req.body['name'];
-    const waiterVentas = await Venta.aggregate(
-        [
-            {
-                $match: {
-                    $and: [
-                        {
-                            date_opened: {
-                                $gte: new Date(inicio + 'Z'),
-                                $lte: new Date(fin + 'Z')
-                            }
-                        },
-                        {
-                            waiter: name
-                        }
-                    ]
-                }
-            },
-        ]
-    );
-    res.json(waiterVentas);
-}
-/**
- * Obtener las ventas registradas por un cajero en un rango de fechas.
- * */
-ventaCtrl.getCashierVentasByDate = async (req, res) => {
-    var inicio = req.body['inicio'];
-    var fin = req.body['fin'];
-    var name = req.body['name'];
-    const cashierVentas = await Venta.aggregate(
-        [
-            {
-                $match: {
-                    $and: [
-                        {
-                            date_opened: {
-                                $gte: new Date(inicio + 'Z'),
-                                $lte: new Date(fin + 'Z')
-                            }
-                        },
-                        {
-                            cashier: name
-                        }
-                    ]
-                }
-            },
-        ]
-    );
-    console.log('Cantidad', cashierVentas.length);
-    res.json(cashierVentas);
-}
+};
+
 /**
  * Obtener los productos mas rentables en un rango de fechas.
  * */
@@ -147,7 +93,8 @@ ventaCtrl.getProductosMasRentables = async (req, res) => {
     console.log('Productos', rankedProducts);
     // console.log('Cantidad', cashierVentas.length);
     res.send(JSON.stringify(rankedProducts));
-}
+};
+
 /**
  * Obtener las categorias de productos mas rentables en un rango de fechas.
  * */
@@ -190,7 +137,8 @@ ventaCtrl.getCategoriasMasRentables = async (req, res) => {
     console.log('Categorias', rankedCategories);
     // console.log('Cantidad', cashierVentas.length);
     res.send(JSON.stringify(rankedCategories));
-}
+};
+
 /**
  * Obtener los productos más vendidos en un rango de fechas.
  * */
@@ -233,13 +181,438 @@ ventaCtrl.getProductosMasVendidos = async (req, res) => {
     console.log('Categorias', rankedProductos);
     // console.log('Cantidad', cashierVentas.length);
     res.send(JSON.stringify(rankedProductos));
-}
+};
 
-ventaCtrl.getRankedWaiters = (req, res) => res.send('Obtener top 10 de meseros con más ventas y rango de fecha');
-ventaCtrl.getRankedCashiers = (req, res) => res.send('Obtener top 10 de cajeros con más ventas y rango de fecha');
-ventaCtrl.getVentasByPaymentMethods = (req, res) => res.send('Obtener el total de ventas segun el metodo de pago y rango de fecha');
-ventaCtrl.getVentasByZone = (req, res) => res.send('Obtener el total de ventas por Zona y rango de fecha');
-ventaCtrl.getVentasByTable = (req, res) => res.send('Obtener el total de ventas por Mesa y rango de fecha');
+/**
+ * Obtener un ranking de Metodos de Pago más rentables y más utilizados en general 
+ * y por zona.
+ * */
+ventaCtrl.getRankedMetodosPago = async (req, res) => {
+    var inicio = req.body['inicio'];
+    var fin = req.body['fin'];
+    // {nombre, cantidad, monto}
+    var rankedMasUsados = [];
+    // {nombre, montoTotal}
+    var rankedMasRentables = [];
+    // {zone, nombre, cantidad, monto}
+    var rankedMasUsadosByZone = [];
+    // {zone, nombre, montoTotal}
+    var rankedMasRentablesByZone = [];
+    var array = [];
+    const ventas = await Venta.aggregate(
+        [
+            {
+                $match: {
+                    date_opened: {
+                        $gte: new Date(inicio + 'Z'),
+                        $lte: new Date(fin + 'Z')
+                    }
+                }
+            },
+        ],
+    );
+    ventas.forEach((venta, i) => {
+        venta.payments.forEach((p) => {
+            let indexRent = rankedMasRentables.findIndex((e) => e.nombre == p.type);
+            if (indexRent != -1) {
+                rankedMasRentables[indexRent].montoTotal += p.amount;
+            } else {
+                rankedMasRentables.push({
+                    "nombre": p.type,
+                    "montoTotal": p.amount
+                });
+            }
+            let indexUsed = rankedMasUsados.findIndex((e) => e.nombre == p.type);
+            if (indexUsed != -1) {
+                rankedMasUsados[indexUsed].cantidad += 1;
+                rankedMasUsados[indexUsed].monto += p.amount;
+            } else {
+                rankedMasUsados.push({
+                    "nombre": p.type,
+                    "cantidad": 1,
+                    "monto": p.amount
+                });
+            }
+            if (venta.zone == 'Salón') {
+                let indexRZ = rankedMasRentablesByZone.findIndex((e) => e.nombre == p.type && e.zone == venta.zone);
+                if (indexRZ != -1) {
+                    rankedMasRentablesByZone[indexRZ].montoTotal += p.amount;
+                } else {
+                    rankedMasRentablesByZone.push({
+                        "nombre": p.type,
+                        "montoTotal": p.amount,
+                        "zone": venta.zone
+                    });
+                }
+                let indexUsed = rankedMasUsadosByZone.findIndex((e) => e.nombre == p.type && e.zone == venta.zone);
+                if (indexUsed != -1) {
+                    rankedMasUsadosByZone[indexUsed].cantidad += 1;
+                    rankedMasUsadosByZone[indexUsed].monto += p.amount;
+                } else {
+                    rankedMasUsadosByZone.push({
+                        "nombre": p.type,
+                        "cantidad": 1,
+                        "zone": venta.zone,
+                        "monto": p.amount
+                    });
+                }
+            } else if (venta.zone == 'Terraza') {
+                let indexRZ = rankedMasRentablesByZone.findIndex((e) => e.nombre == p.type && e.zone == venta.zone);
+                if (indexRZ != -1) {
+                    rankedMasRentablesByZone[indexRZ].montoTotal += p.amount;
+                } else {
+                    rankedMasRentablesByZone.push({
+                        "nombre": p.type,
+                        "montoTotal": p.amount,
+                        "zone": venta.zone
+                    });
+                }
+                let indexUsed = rankedMasUsadosByZone.findIndex((e) => e.nombre == p.type && e.zone == venta.zone);
+                if (indexUsed != -1) {
+                    rankedMasUsadosByZone[indexUsed].cantidad += 1;
+                } else {
+                    rankedMasUsadosByZone.push({
+                        "nombre": p.type,
+                        "cantidad": 1,
+                        "zone": venta.zone
+                    });
+                }
+            } else {
+                let indexRZ = rankedMasRentablesByZone.findIndex((e) => e.nombre == p.type && e.zone == venta.zone);
+                if (indexRZ != -1) {
+                    rankedMasRentablesByZone[indexRZ].montoTotal += p.amount;
+                } else {
+                    rankedMasRentablesByZone.push({
+                        "nombre": p.type,
+                        "montoTotal": p.amount,
+                        "zone": venta.zone
+                    });
+                }
+                let indexUsed = rankedMasUsadosByZone.findIndex((e) => e.nombre == p.type && e.zone == venta.zone);
+                if (indexUsed != -1) {
+                    rankedMasUsadosByZone[indexUsed].cantidad += 1;
+                } else {
+                    rankedMasUsadosByZone.push({
+                        "nombre": p.type,
+                        "cantidad": 1,
+                        "zone": venta.zone
+                    });
+                }
+            }
+        });
+    });
+    // Ordernar DESC
+    rankedMasRentables.sort(function (b, a) {
+        if (a.montoTotal > b.montoTotal) return 1;
+        if (a.montoTotal < b.montoTotal) return -1;
+        return 0;
+    });
+    // Ordernar DESC
+    rankedMasUsados.sort(function (b, a) {
+        if (a.cantidad > b.cantidad) return 1;
+        if (a.cantidad < b.cantidad) return -1;
+        return 0;
+    });
+    // Ordernar DESC
+    rankedMasRentablesByZone.sort(function (b, a) {
+        if (a.montoTotal > b.montoTotal) return 1;
+        if (a.montoTotal < b.montoTotal) return -1;
+        return 0;
+    });
+    // Ordernar DESC
+    rankedMasUsadosByZone.sort(function (b, a) {
+        if (a.cantidad > b.cantidad) return 1;
+        if (a.cantidad < b.cantidad) return -1;
+        return 0;
+    });
+    array.push({
+        "masUsados": rankedMasUsados,
+        "masRentables": rankedMasRentables,
+        "masUsadosByZone": rankedMasUsadosByZone,
+        "masRentablesByZone": rankedMasRentablesByZone,
+    });
+    res.json(array);
+};
+
+/**
+ * Obtener total de ventas por zona ordenadas de forma DESC en un rango de 
+ * fechas.
+ * */
+ventaCtrl.getVentasByZone = async (req, res) => {
+    var inicio = req.body['inicio'];
+    var fin = req.body['fin'];
+    // {zone, montoTotal}
+    var rankedZones = [];
+    const ventas = await Venta.aggregate(
+        [
+            {
+                $match: {
+                    date_opened: {
+                        $gte: new Date(inicio + 'Z'),
+                        $lte: new Date(fin + 'Z')
+                    }
+                }
+            },
+        ],
+    );
+    ventas.forEach((venta, i) => {
+        let index = rankedZones.findIndex((e) => e.zone == venta.zone);
+        if (index != -1) {
+            rankedZones[index].montoTotal += venta.total;
+        } else {
+            rankedZones.push({
+                "zone": venta.zone,
+                "montoTotal": venta.total
+            });
+        }
+    });
+    // Ordernar DESC
+    rankedZones.sort(function (b, a) {
+        if (a.montoTotal > b.montoTotal) return 1;
+        if (a.montoTotal < b.montoTotal) return -1;
+        return 0;
+    });
+    res.json(rankedZones);
+};
+
+/**
+ * Obtener total de ingresos ordenados por dia de la semana (Lun-Dom) en un rango de 
+ * fechas. {dia, montoTotal}
+ * */
+ventaCtrl.getIngresoPorDia = async (req, res) => {
+    var inicio = req.body['inicio'];
+    var fin = req.body['fin'];
+    // {dia, montoTotal}
+    var ingresosPerDay = [];
+    var days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado'];
+    const ventas = await Venta.aggregate(
+        [
+            {
+                $match: {
+                    $and: [
+                        {
+                            date_opened: {
+                                $gte: new Date(inicio + 'Z'),
+                                $lte: new Date(fin + 'Z')
+                            }
+                        }
+                    ]
+                }
+            },
+        ],
+    );
+    ventas.forEach((venta) => {
+        var d = venta.date_opened;
+        var dayName = days[d.getDay()];
+        let index = ingresosPerDay.findIndex((e) => e.dia == dayname);
+        if (index != -1) {
+            ingresosPerDay[index].montoTotal += venta.total;
+        } else {
+            ingresosPerDay.push({
+                "dia": dayname,
+                "montoTotal": venta.total
+            });
+        }
+        console.log(dayName);
+    });
+    res.json(ingresosPerDay);
+};
+// 
+// ---------------------Reporte de Trabajadores---------------------------
+/**
+ * Obtener las ventas atendidas por un mesero en un rango de fechas.
+ * */
+ventaCtrl.getWaiterVentasByDate = async (req, res) => {
+    var inicio = req.body['inicio'];
+    var fin = req.body['fin'];
+    var name = req.body['name'];
+    const waiterVentas = await Venta.aggregate(
+        [
+            {
+                $match: {
+                    $and: [
+                        {
+                            date_opened: {
+                                $gte: new Date(inicio + 'Z'),
+                                $lte: new Date(fin + 'Z')
+                            }
+                        },
+                        {
+                            waiter: name
+                        }
+                    ]
+                }
+            },
+        ]
+    );
+    res.json(waiterVentas);
+};
+
+/**
+ * Obtener las ventas registradas por un cajero en un rango de fechas.
+ * */
+ventaCtrl.getCashierVentasByDate = async (req, res) => {
+    var inicio = req.body['inicio'];
+    var fin = req.body['fin'];
+    var name = req.body['name'];
+    const cashierVentas = await Venta.aggregate(
+        [
+            {
+                $match: {
+                    $and: [
+                        {
+                            date_opened: {
+                                $gte: new Date(inicio + 'Z'),
+                                $lte: new Date(fin + 'Z')
+                            }
+                        },
+                        {
+                            cashier: name
+                        }
+                    ]
+                }
+            },
+        ]
+    );
+    console.log('Cantidad', cashierVentas.length);
+    res.json(cashierVentas);
+};
+
+/**
+ * Obtener un top de meseros en un rango de fechas por: rentabilidad, personas atendidas y ventas realizadas. 
+ * En general y filtradas por Zona
+ * */
+ventaCtrl.getRankedWaiters = async (req, res) => {
+    var inicio = req.body['inicio'];
+    var fin = req.body['fin'];
+    // {nombre, cantidad }
+    var waitersMasRentables = [];
+    var waitersMasPersonasAtendidas = [];
+    var waitersMasVentas = [];
+    // {nombre, cantidad, zone }
+    var waitersMasRentablesByZone = [];
+    var waitersMasPersonasAtendidasByZone = [];
+    var waitersMasVentasByZone = [];
+    var array = [];
+    const ventas = await Venta.aggregate(
+        [
+            {
+                $match: {
+                    date_opened: {
+                        $gte: new Date(inicio + 'Z'),
+                        $lte: new Date(fin + 'Z')
+                    }
+                }
+            },
+        ],
+    );
+    ventas.forEach((venta, i) => {
+        let indexR = waitersMasRentables.findIndex((e) => e.nombre == venta.waiter);
+        if (indexR != -1) {
+            waitersMasRentables[indexR].cantidad += venta.total;
+        } else {
+            waitersMasRentables.push({
+                "nombre": venta.waiter,
+                "cantidad": venta.total
+            });
+        }
+        let indexPA = waitersMasPersonasAtendidas.findIndex((e) => e.nombre == venta.waiter);
+        if (indexPA != -1) {
+            waitersMasPersonasAtendidas[indexPA].cantidad += venta.diners;
+        } else {
+            waitersMasPersonasAtendidas.push({
+                "nombre": venta.waiter,
+                "cantidad": venta.diners
+            });
+        }
+        let indexPR = waitersMasVentas.findIndex((e) => e.nombre == venta.waiter);
+        if (indexPR != -1) {
+            waitersMasVentas[indexPR].cantidad += 1;
+        } else {
+            waitersMasVentas.push({
+                "nombre": venta.waiter,
+                "cantidad": 1
+            });
+        }
+        let indexRZ = waitersMasRentablesByZone.findIndex((e) => e.nombre == venta.waiter && e.zone == venta.zone);
+        if (indexRZ != -1) {
+            waitersMasRentablesByZone[indexRZ].cantidad += venta.total;
+        } else {
+            waitersMasRentablesByZone.push({
+                "nombre": venta.waiter,
+                "cantidad": venta.total,
+                "zone": venta.zone
+            });
+        }
+        let indexPAZ = waitersMasPersonasAtendidasByZone.findIndex((e) => e.nombre == venta.waiter && e.zone == venta.zone);
+        if (indexPAZ != -1) {
+            waitersMasPersonasAtendidasByZone[indexPAZ].cantidad += venta.diners;
+        } else {
+            waitersMasPersonasAtendidasByZone.push({
+                "nombre": venta.waiter,
+                "cantidad": venta.diners,
+                "zone": venta.zone
+            });
+        }
+        let indexPRZ = waitersMasVentasByZone.findIndex((e) => e.nombre == venta.waiter && e.zone == venta.zone);
+        if (indexPRZ != -1) {
+            waitersMasVentasByZone[indexPRZ].cantidad += 1;
+        } else {
+            waitersMasVentasByZone.push({
+                "nombre": venta.waiter,
+                "cantidad": 1,
+                "zone": venta.zone
+            });
+        }
+    });
+    // Ordernar DESC
+    waitersMasRentables.sort(function (b, a) {
+        if (a.cantidad > b.cantidad) return 1;
+        if (a.cantidad < b.cantidad) return -1;
+        return 0;
+    });
+    // Ordernar DESC
+    waitersMasPersonasAtendidas.sort(function (b, a) {
+        if (a.cantidad > b.cantidad) return 1;
+        if (a.cantidad < b.cantidad) return -1;
+        return 0;
+    });
+    // Ordernar DESC
+    waitersMasVentas.sort(function (b, a) {
+        if (a.cantidad > b.cantidad) return 1;
+        if (a.cantidad < b.cantidad) return -1;
+        return 0;
+    });
+    // Ordernar DESC
+    waitersMasRentablesByZone.sort(function (b, a) {
+        if (a.cantidad > b.cantidad) return 1;
+        if (a.cantidad < b.cantidad) return -1;
+        return 0;
+    });
+    // Ordernar DESC
+    waitersMasPersonasAtendidasByZone.sort(function (b, a) {
+        if (a.cantidad > b.cantidad) return 1;
+        if (a.cantidad < b.cantidad) return -1;
+        return 0;
+    });
+    // Ordernar DESC
+    waitersMasVentasByZone.sort(function (b, a) {
+        if (a.cantidad > b.cantidad) return 1;
+        if (a.cantidad < b.cantidad) return -1;
+        return 0;
+    });
+    array.push({
+        "rentables": waitersMasRentables,
+        "masPersonas": waitersMasPersonasAtendidas,
+        "masPedidos": waitersMasVentas,
+        "rentablesByZone": waitersMasRentablesByZone,
+        "masPersonasByZone": waitersMasPersonasAtendidasByZone,
+        "masPedidosByZone": waitersMasVentasByZone,
+    });
+    res.json(array);
+};
+
+// ventaCtrl.getVentasByTable = (req, res) => res.send('Obtener el total de ventas por Mesa y rango de fecha');
 // Cambiar el date tipo String a tipo Date. (Se utilizo solo 1 vez)
 ventaCtrl.changeStringToDate = async (req, res) => {
     await Venta.collection.find().forEach(function (obj) {
--- src/routes/ventas.routes.js
+++ src/routes/ventas.routes.js
@@ -4,15 +4,16 @@ const router = Router();
 const ventaCtrl = require('../controllers/ventas.controller.js');
 // aqui van todas las rutas de la REST API
 // router.get('/', ventaCtrl.getVentasByDate);
-// router.get('/', ventaCtrl.getTotalVentasByDate);
+// router.get('/', ventaCtrl.getIngresoTotalByDate);
 // router.get('/', ventaCtrl.getWaiterVentasByDate);
+// router.get('/', ventaCtrl.getRankedWaiters);
 // router.get('/', ventaCtrl.getCashierVentasByDate);
 // router.get('/', ventaCtrl.getProductosMasRentables);
 // router.get('/', ventaCtrl.getCategoriasMasRentables);
-router.get('/', ventaCtrl.getProductosMasVendidos);
+// router.get('/', ventaCtrl.getProductosMasVendidos);
+router.get('/', ventaCtrl.getRankedMetodosPago);
 // router.get('/', ventaCtrl.getVentasByZone);
 // router.get('/', ventaCtrl.getVentasByTable);
-// router.get('/', ventaCtrl.getRankedWaiters);
 // router.get('/', ventaCtrl.getRankedCashiers);
 // router.get('/', ventaCtrl.getVentasByPaymentMethods);
 
